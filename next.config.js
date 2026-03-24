6bec018e52eb1dbe74dadfb0564056ddb2d9fd3a
GrangeAGBODANDE <agbodande98@gmail.com>
2026-03-24 16:28:35 +0100

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: [
+      'uattwwcdchfxxpjfqdtm.supabase.co',
+      'via.placeholder.com',
+      'images.unsplash.com',
+      'img.youtube.com'
+    ],
+  },
+}
+
+module.exports = nextConfig
